9902c5a8ef26feddf12afdd4bee58c3d1316580b
fedul0x <fedul0x@linux-xhuq.site>
2012-07-27 01:40:13 +0400

    Счетчик слов в директориях теперь пишет в ~/tmp

--- symbol_counter_for_directory.sh
+++ symbol_counter_for_directory.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #Добавить проверку исходных данных
-
-old_value=$(find "$1" -iname "*$2*" -exec cat '{}' \; | wc -m)
+output_file=~/tmp/symbol_counter_for_directory_output
+old_value=$(find "$1" -iname "*$2*" -exec cat '{}' \; | wc -w)
 while true
 do
-echo "Symbols init value: "$old_value
-new_value=$(find "$1" -iname "*$2*" -exec cat '{}' \; | wc -m)
-echo "Symbols init value: "$new_value
+echo "Symbols init value: "$old_value > $output_file
+new_value=$(find "$1" -iname "*$2*" -exec cat '{}' \; | wc -w)
+echo "Symbols init value: "$new_value >> $output_file
 let d=$new_value-$old_value
-echo "Diff value: "$d
+echo "Diff value: "$d >> $output_file
 sleep 60
 done
